3e5b89739ef680d8c43209a4b9a305803176c593
alexs <sandroberidze65@gmail.com>
2025-01-21 03:31:52 +0400

    added domain layer with entities, value object,  interfaces, domain event and services, and errors

--- Bookify.Domain/Abstractions/Entity.cs
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -2,10 +2,26 @@
 
 public abstract class Entity
 {
+    private readonly List<IDomainEvent> _domainEvents = new();
     public Guid Id { get; init; }
 
     protected Entity(Guid id)
     {
         Id = id;
+    }
+
+    public IReadOnlyList<IDomainEvent> DomainEvents()
+    {
+        return _domainEvents.ToList();
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
     }
 }
---
+++ Bookify.Domain/Bookings/PricingDetails.cs
@@ -0,0 +1,10 @@
+﻿using Bookify.Domain.Shared;
+
+namespace Bookify.Domain.Bookings;
+
+public record PricingDetails(
+     Money priceForPeriod,
+     Money cleanigfee,
+     Money amenitiesUpCharge,
+     Money totalPrice
+);
